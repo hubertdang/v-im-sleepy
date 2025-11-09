0c35720e15cefd792becb92d7729d830448a672b
Hubert Dang <hubertdang@gmail.com>
2025-11-09 16:50:56 -0500

    Add auto-complete

---
+++ after/plugin/cmp.lua
@@ -0,0 +1,20 @@
+-- ~/.config/nvim/after/plugin/cmp.lua
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- You can integrate a snippet engine here if you want
+      -- For now, just leave it empty
+    end,
+  },
+  mapping = {
+    ['j'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['k'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  },
+})
--- lua/hubertdang/packer.lua
+++ lua/hubertdang/packer.lua
@@ -29,4 +29,13 @@ return require('packer').startup(function(use)
 	  'neovim/nvim-lspconfig', -- collection of common LSP configs
   }
 
+  -- Completion framework
+  use 'hrsh7th/nvim-cmp'          -- Core completion plugin
+  use 'hrsh7th/cmp-nvim-lsp'      -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-buffer'        -- Buffer completions
+  use 'hrsh7th/cmp-path'          -- File path completions
+  use 'hrsh7th/cmp-cmdline'       -- Command line completions
+  use 'L3MON4D3/LuaSnip'          -- Snippet engine
+  use 'saadparwaiz1/cmp_luasnip'  -- Snippet completions
+
 end)
