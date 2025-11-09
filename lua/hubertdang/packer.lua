5a0c6e654b0749ff560a839ea9bd32c739ea5d36
Hubert Dang <hubertdang@gmail.com>
2025-11-09 10:07:47 -0500

    Remove disable function for treesitter I don't want

--- lua/hubertdang/packer.lua
+++ lua/hubertdang/packer.lua
@@ -25,15 +25,6 @@ return require('packer').startup(function(use)
 	  run = ':TSUpdate'
   }
 
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
   use {
 	  'neovim/nvim-lspconfig', -- collection of common LSP configs
   }
